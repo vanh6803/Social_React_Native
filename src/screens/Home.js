b2b4869bb8f07a72754d8d6537e4778f09fca3a3
vanh6803 <anhnv6083@gmail.com>
2023-10-16 13:23:52 +0700

    hidden bottom when scroll flatlist

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,13 +10,12 @@ import PagerView from 'react-native-pager-view';
 
 const Home = () => {
   const pageRef = useRef(null);
+  const flatListRef = useRef(null);
   const [currentPage, setCurrentPage] = useState(0);
-  const [dataPosts, setDataPosts] = useState(null);
   const posts = useSelector(state => state.postsReducer.posts);
   const loading = useSelector(state => state.postsReducer.loading);
   const error = useSelector(state => state.postsReducer.error);
   const dispatch = useDispatch();
-  const [pagerHeight, setPagerHeight] = useState(0);
 
   const navigation = useNavigation();
 
@@ -24,12 +23,47 @@ const Home = () => {
     dispatch(fetchPostsRequest());
     console.log('posts', posts.posts);
   }, [dispatch]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      // Mặc định, hiển thị bottom tab khi màn hình Home có focus
+      navigation.setOptions({
+        tabBarVisible: true,
+      });
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  let previousOffsetY = 0;
+
+  const handleScroll = event => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+
+    if (offsetY > previousOffsetY) {
+      // `offsetY` đang tăng, ẩn bottom tab
+      navigation.setOptions({
+        tabBarVisible: false,
+      });
+    } else if (offsetY < previousOffsetY) {
+      // `offsetY` đang giảm, hiển thị bottom tab
+      navigation.setOptions({
+        tabBarVisible: true,
+      });
+    }
+
+    // Lưu giá trị `offsetY` hiện tại cho lần so sánh tiếp theo
+    previousOffsetY = offsetY;
+  };
+
   return (
     <View className="flex-1">
       <TopBar />
       <FlatList
         className="flex-1"
         data={posts}
+        ref={flatListRef}
+        onScroll={handleScroll}
         keyExtractor={item => item._id}
         renderItem={({item}) => {
           return (
